557c031a109e3f17f23b092b9abab438c3ce74c3
Anuja Rathnayaka <anujanisal19@gmail.com>
2025-02-26 16:48:37 +0530

    chore(routes): react routes added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
+import Challenges from './pages/Challenges';
+
 function App() {
-  return <h1 className="text-3xl font-bold underline text-text">Hello world!</h1>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/challenges" element={<Challenges />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
