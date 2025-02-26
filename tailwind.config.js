6ccc2cfb11fd42155d5a5ef482164c377f4b5da2
Anuja Rathnayaka <anujanisal19@gmail.com>
2025-02-26 16:37:07 +0530

    chore(tailwind): tailwind color var

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        textGray: '#585656',
+        primaryGreen: '#1BAC5F',
+        white: '#FFFFFF',
+        red: '#E15959',
+        outlineGray: '#DBDBDB',
+        bgLightGray: '#F8F8F8',
+      },
+    },
   },
   plugins: [],
-}
+};
